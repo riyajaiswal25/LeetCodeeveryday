bebd4aec995303f38c9f8ee60dfaf97e695f609b
Riya Jaiswal <84279900+riyajaiswal25@users.noreply.github.com>
2022-09-18 20:00:10 +0530

    Create Trappingrainwater.java

---
+++ Trappingrainwater.java
@@ -0,0 +1,30 @@
+//Using two pointer solution
+class Solution {
+    public int trap(int[] height) {
+        int n=height.length;
+        int l=0;
+        int r=n-1;
+        int leftmax=0;
+        int rightmax=0;
+        int res=0;
+        while(l<=r)
+        {
+            if(height[l]<=height[r])
+            {
+                if(height[l]>leftmax)
+                    leftmax=height[l];
+                else
+                    res+=leftmax-height[l];
+                l++;
+            }
+            else{
+                if(height[r]>=rightmax)
+                    rightmax=height[r];
+                else
+                    res+=rightmax-height[r];
+                r--;
+            }
+        }
+        return res;
+    }
+}
