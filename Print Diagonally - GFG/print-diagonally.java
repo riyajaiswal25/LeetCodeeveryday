e8802d6b74c312303d084b3a0ed6a5fb039025f9
Riya Jaiswal <84279900+riyajaiswal25@users.noreply.github.com>
2022-10-18 17:54:36 +0530

    Added solution - LeetHub

---
+++ Print Diagonally - GFG/print-diagonally.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            
+            int matrix[][] = new int[n][n];
+            
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                 matrix[i][j] = sc.nextInt();
+            }
+            Solution ob = new Solution();
+            ArrayList<Integer> ans = ob.downwardDigonal(n, matrix);
+            for (Integer val: ans) 
+                System.out.print(val+" "); 
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static ArrayList<Integer> downwardDigonal(int N, int A[][])
+    {
+        // code here 
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        for(int i=0;i<=(N-1)*2;i++)
+        for(int j=0;i-j>=0;j++)
+        if(j<N && i-j<N)
+        list.add(A[j][i-j]);
+        return list;
+    }
+}
