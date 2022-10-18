0da59032b8a4c7a53e3b0f643f8e48de88c6e3f7
Riya Jaiswal <84279900+riyajaiswal25@users.noreply.github.com>
2022-10-18 22:11:14 +0530

    Added solution - LeetHub

--- Print Diagonally - GFG/print-diagonally.java
+++ Print Diagonally - GFG/print-diagonally.java
@@ -38,11 +38,34 @@ class Solution{
     static ArrayList<Integer> downwardDigonal(int N, int A[][])
     {
         // code here 
-        ArrayList<Integer> list=new ArrayList<Integer>();
-        for(int i=0;i<=(N-1)*2;i++)
-        for(int j=0;i-j>=0;j++)
-        if(j<N && i-j<N)
-        list.add(A[j][i-j]);
-        return list;
+        ArrayList<Integer> ans=new ArrayList<Integer>();
+       int row=0;
+       int col=0;
+       while(col<N)
+       {
+           int r=row;
+           int c=col;
+           while(r<N && c>=0)
+           {
+               ans.add(A[r][c]);
+               r++;
+               c--;
+           }
+           col++;
+       }
+       col=N-1;
+       row=1;
+       while(row<N)
+       {
+           int r=row;
+           int c=col;
+           while(r<N && c>=0){
+               ans.add(A[r][c]);
+               r++;
+               c--;
+           }
+           row++;
+       }
+        return ans;
     }
 }
