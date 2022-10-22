f4c3fbe1161a2b79950bdc2d2395e576940d1e61
Riya Jaiswal <84279900+riyajaiswal25@users.noreply.github.com>
2022-10-23 00:35:57 +0530

    Time: 341 ms (8.36%), Space: 327.1 MB (5.01%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,52 +1,57 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(s == null || s.isEmpty() || t == null || t.isEmpty()) return "";
-        int[] Tmap = new int[256];
-        int[] Smap = new int[256];
-        int left=0, right = 0;
-        //recording the fre of each char from t
-        for(int k = 0; k<t.length(); k++)
-            Tmap[t.charAt(k)]++;
-        
-        int found = 0;
-        int length = Integer.MAX_VALUE;
-        
-        String res = "";
-        while(right < s.length()){
-            //right forward
-            if(found < t.length()){
-                //move keep traversing
-                if(Tmap[s.charAt(right)] > 0){
-                    Smap[s.charAt(right)] ++;
-                    if(Smap[s.charAt(right)] <= Tmap[s.charAt(right)]){
-                        //This char is a target char and the fre is lower target fre
-                        //We found it
-                        found++;
-                    }
-                }
-                right++;
-            }
-            //if the found equals to target length
-            //if the found still equals to the target length, keep deleting (moving left forward)
-            while(found == t.length()){
-                //if get the shorter substring
-                if(right - left < length){
-                    length = right - left;
-                    res = s.substring(left,right);
-                }
-                //move left forward
-                if(Tmap[s.charAt(left)] > 0){
-                    //fre of Smap decrease
-                    Smap[s.charAt(left)]--;
-                    //we delete a char
-                    if(Smap[s.charAt(left)] < Tmap[s.charAt(left)]){
-                        found--;
-                    }
-                }
-                //move left forward
-                left++;
-            }
+        String ans="";
+        HashMap<Character, Integer> mapt = new HashMap<>();
+        for(int i=0;i<t.length();i++)
+        {
+            char ch=t.charAt(i);
+            mapt.put(ch, mapt.getOrDefault(ch,0)+1);
         }
-        return res;
+        int mct=0;
+        int dmct=t.length();
+        HashMap<Character, Integer> maps = new HashMap<>();
+        int i=-1;
+        int j=-1;
+        while(true)
+        {
+            boolean f1=false;
+            boolean f2=false;
+            while(i<s.length()-1 && mct<dmct)
+            {
+                i++;
+                char ch=s.charAt(i);
+                maps.put(ch,maps.getOrDefault(ch,0)+1);
+                if(maps.getOrDefault(ch,0)<=mapt.getOrDefault(ch,0))
+                {
+                    mct++;
+                }
+                f1=true;
+            }
+            while(j<i && mct==dmct)
+            {
+                String pans=s.substring(j+1,i+1);
+                if(ans.length()==0 || pans.length()<ans.length())
+                {
+                    ans=pans;
+                }
+                j++;
+                char ch=s.charAt(j);
+                if(maps.get(ch)==1)
+                {
+                    maps.remove(ch);
+                }
+                else{
+                    maps.put(ch,maps.get(ch)-1);
+                }
+                if(maps.getOrDefault(ch,0)<mapt.getOrDefault(ch,0))
+                {
+                    mct--;
+                }
+                f2=true;
+            }
+            if(f1==false && f2==false)
+                break;
+        }
+        return ans;
     }
 }
