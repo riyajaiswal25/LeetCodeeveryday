af4a7bdb46864a1daae21fa59a6c952af03d46f5
Riya Jaiswal <84279900+riyajaiswal25@users.noreply.github.com>
2022-11-10 18:49:22 +0530

    Added solution - LeetHub

---
+++ Jumping Numbers - GFG/jumping-numbers.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            long X = Long.parseLong(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.jumpingNums(X));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static long jumpingNums(long X) {
+        // code here
+        Queue<Long> q = new LinkedList<>();
+        for(long i=1;i<=9;i++)
+        {
+            q.add(i);
+        }
+        long ans=0;
+        while(!q.isEmpty())
+        {
+            long num=q.remove();
+            if(num>X)
+            {
+                continue;
+            }
+            ans=Math.max(ans,num);
+            int ld=(int)num%10;
+            if(ld!=0)
+            {
+                long f=num*10+(ld-1);
+                q.add(f);
+            }
+            if(ld!=9)
+            {
+                long s=num*10+(ld+1);
+                q.add(s);
+            }
+        }
+        return ans;
+    }
+};
