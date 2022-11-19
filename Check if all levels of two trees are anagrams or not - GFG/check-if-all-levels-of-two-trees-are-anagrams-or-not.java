94bc75f020f1213114edfd2e76be084b4de383e6
Riya Jaiswal <84279900+riyajaiswal25@users.noreply.github.com>
2022-11-19 19:30:35 +0530

    Added solution - LeetHub

---
+++ Check if all levels of two trees are anagrams or not - GFG/check-if-all-levels-of-two-trees-are-anagrams-or-not.java
@@ -0,0 +1,187 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+    
+    public static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length()==0 || str.charAt(0)=='N')
+            return null;
+        
+        // Creating array of Strings from input 
+        // String after spliting by space
+        String ip[] = str.split(" ");
+        
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        
+        // Push the root to the queue
+        Queue<Node> queue = new LinkedList<>(); 
+        queue.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(queue.size()>0 && i < ip.length)
+        {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N"))
+            {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N"))
+            {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+    public static Node inputTree(BufferedReader br) throws IOException
+    {
+        return buildTree(br.readLine().trim());
+    }
+    
+    public static void inorder(Node root)
+    {
+        if (root == null)
+           return;
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            Node node1 = Node.inputTree(br);
+            
+            
+            Node node2 = Node.inputTree(br);
+            
+            Solution obj = new Solution();
+            boolean res = obj.areAnagrams(node1, node2);
+            
+            int _result_val = (res) ? 1 : 0;
+            System.out.println(_result_val);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+                                                                         
+Definition for Binary Tree Node
+class Node
+{
+    int data;
+    Node left;                                                                                                                                                              
+    Node right;
+    
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+    public static boolean areAnagrams(Node node1, Node node2) {
+        // code here
+        Queue<Node> a=new LinkedList<>();
+        Queue<Node> b=new LinkedList<>();
+        
+        a.add(node1);
+        b.add(node2);
+        
+        Set<Integer> set=new HashSet<>();
+        while(a.size()>0 && b.size()>0)
+        {
+            int size=a.size();
+            while(size-- > 0)
+            {
+                Node nd=a.remove();
+                set.add(nd.data);
+                if(nd.left!=null)
+                {
+                    a.offer(nd.left);
+                }
+                if(nd.right!=null)
+                {
+                    a.offer(nd.right);
+                }
+            }
+            size=b.size();
+            while(size-- > 0)
+            {
+                Node nd=b.remove();
+                if(set.contains(nd.data))
+                {
+                    set.remove(nd.data);
+                }
+                if(nd.left!=null)
+                {
+                    b.offer(nd.left);
+                }
+                if(nd.right!=null)
+                {
+                    b.offer(nd.right);
+                }
+            }
+            if(set.size()>0)
+            return false;
+        }
+        return set.size()>0?false:true;
+    }
+}
+        
