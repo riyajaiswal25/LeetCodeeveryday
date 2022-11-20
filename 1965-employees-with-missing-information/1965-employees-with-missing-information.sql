fe271550010f2ab684a6d351c3ff8c211adfa555
Riya Jaiswal <84279900+riyajaiswal25@users.noreply.github.com>
2022-11-20 11:38:11 +0530

    Time: 723 ms (38.40%), Space: 0B (100.00%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select employee_id from Salaries
+where employee_id  not in (select employee_id  from Employees)
+union 
+select employee_id  from Employees 
+where employee_id  not in (select employee_id  from Salaries)
+order by employee_id asc;
