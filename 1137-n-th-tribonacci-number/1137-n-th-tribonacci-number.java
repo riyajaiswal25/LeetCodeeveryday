77cd5022d238d7a57a02bcb4d9c3e83b607b879b
Riya Jaiswal <84279900+riyajaiswal25@users.noreply.github.com>
2023-01-30 11:59:24 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (90.39%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int tribonacci(int n) {
+        int arr[] = new int[]{0,1,1};
+        for(int i=3;i<=n;i++){
+            arr[i%3]=arr[0]+arr[1]+arr[2];
+        }
+        return arr[n%3];
+    }
+}
