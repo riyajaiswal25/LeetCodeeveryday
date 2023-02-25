027b2799a8ddf34e9d7dec686eeb62b27c60e7f9
Riya Jaiswal <84279900+riyajaiswal25@users.noreply.github.com>
2023-02-25 22:41:58 +0530

    Added solution - LeetHub

---
+++ Print first letter of every word in the string - GFG/print-first-letter-of-every-word-in-the-string.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+            Solution ob = new Solution();
+            String result = ob.firstAlphabet(S);
+            
+            System.out.println(result);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    String firstAlphabet(String S) {
+        // code here
+        String res="";
+        res=res+S.charAt(0);
+        for(int i=1;i<S.length();i++)
+        {
+            if(S.charAt(i)==' ')
+            res=res+S.charAt(i+1);
+        }
+        return res;
+    }
+};
