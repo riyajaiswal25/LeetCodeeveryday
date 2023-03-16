4e9bdc2a11d3a3ad98842ec90bf9919e0abb91a5
Riya Jaiswal <84279900+riyajaiswal25@users.noreply.github.com>
2023-03-17 00:25:11 +0530

    Added solution - LeetHub

---
+++ First and last occurrences of X - GFG/first-and-last-occurrences-of-x.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            String inputLine[] = br.readLine().trim().split(" "); 
+            int n = Integer.parseInt(inputLine[0]); 
+            int x = Integer.parseInt(inputLine[1]);
+            
+            int arr[] = new int[(int)(n)];
+            
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine1[i]);
+            }
+            
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.firstAndLast(arr, n, x); 
+            
+            for (Integer val: ans) 
+                System.out.print(val+" "); 
+            System.out.println();
+        }
+    } 
+} 
+            
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    public ArrayList<Integer> firstAndLast(int arr[], int n, int x){
+        // Code here
+        ArrayList<Integer> ans=new ArrayList<>();
+        int c=0, j=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==x)
+            {
+                c++;
+                if(c==1)
+                ans.add(i);
+                j=i;
+            }
+        }
+        if(ans.size()==1)
+        ans.add(j);
+        if(ans.size()==0)
+        {
+            ans.add(-1);
+            return ans;
+        }
+        return ans;
+    }
+}
