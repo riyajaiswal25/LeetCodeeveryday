6a735924d076d218a64f756ce52d14fab52cb36b
Riya Jaiswal <84279900+riyajaiswal25@users.noreply.github.com>
2023-04-16 21:08:51 +0530

    Time: 41 ms (49.58%), Space: 53.7 MB (25.03%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,13 +1,34 @@
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        int size=nums.length-(k-1);
-        PriorityQueue<Integer> pq=new PriorityQueue<Integer>(Collections.reverseOrder());
-        for(int num:nums)
-        {
-            pq.offer(num);
-            if(pq.size()>size)
-                pq.poll();
+    void swap(int arr[],int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    int partition(int nums[],int low,int high){
+        int pivot=nums[high];
+        int i=low-1,j=low;
+        while(j<high){
+            if(nums[j]<pivot){
+                i++;
+                swap(nums,i,j);
+            }
+            j++;
         }
-        return pq.poll();
+        swap(nums,i+1,j);
+        return i+1;
+    }
+    public int findKthLargest(int[] nums, int k) {
+        int low=0,high=nums.length-1;
+        k=nums.length-k;
+        while(true){
+            int temp=partition(nums,low,high);
+            System.out.print(temp+" ");
+            if(temp==k)
+            return nums[temp];
+            else if(temp<k)
+            low=temp+1;
+            else
+            high=temp-1;
+        }
     }
 }
