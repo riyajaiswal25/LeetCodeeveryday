c1459f0f771ea0b6848427a77d622268eadf79be
Riya Jaiswal <84279900+riyajaiswal25@users.noreply.github.com>
2023-06-19 09:33:29 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (48.11%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int max=0, sum=0;
+        for(int i:gain)
+        {
+            sum+=i;
+            max=Math.max(max,sum);
+        }
+        return max;
+         
+
+        
+    }
+}
