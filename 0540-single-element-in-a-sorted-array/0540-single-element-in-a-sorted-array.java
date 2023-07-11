1dcabb29d33bb48c4a25fd12f87d93aca69bdd3b
Riya Jaiswal <84279900+riyajaiswal25@users.noreply.github.com>
2023-07-11 13:13:47 +0530

    Time: 0 ms (100.00%), Space: 50 MB (60.00%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -3,7 +3,7 @@ class Solution {
         int l=0, r=nums.length-1;
         while(l<r)
         {
-            int mid=(l+r)/2;
+            int mid = l + (r-l)/2;
             if(mid%2 == 1)
                 mid--;
             if(nums[mid]==nums[mid+1])
