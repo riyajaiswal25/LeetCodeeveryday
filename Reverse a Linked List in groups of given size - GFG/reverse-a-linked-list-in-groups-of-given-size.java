e8048369ee629f9ee1f2c535dc620f1f79449be6
Riya Jaiswal <84279900+riyajaiswal25@users.noreply.github.com>
2023-07-21 12:47:16 +0530

    Added solution - LeetHub

---
+++ Reverse a Linked List in groups of given size - GFG/reverse-a-linked-list-in-groups-of-given-size.java
@@ -0,0 +1,103 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class ReverseInSize
+{
+    static Node head;
+    
+    public static void main (String[] args)throws IOException {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        
+        int t = Integer.parseInt(in.readLine().trim());
+        
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(in.readLine().trim());
+            String s[]=in.readLine().trim().split(" ");
+            int a1 = Integer.parseInt(s[0]);
+            Node head = new Node(a1);
+            Node tail = head;
+            for(int i = 1; i < n; i++)
+            {
+                int a = Integer.parseInt(s[i]);
+                // addToTheLast(new Node(a));
+                tail.next = new Node(a);
+                tail =tail.next;
+            }
+            
+            int k = Integer.parseInt(in.readLine().trim());
+            Solution ob = new Solution();
+            Node res = ob.reverse(head, k);
+            printList(res,out);
+            out.println();
+        }
+        out.close();
+    }
+    
+    public static void printList(Node node,PrintWriter out)
+    {
+        while(node != null)
+        {
+            out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+}
+
+
+// } Driver Code Ends
+
+
+/*node class of the linked list
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+*/
+
+class Solution
+{
+    public static Node reverse(Node head, int k)
+    {
+        //Your code here
+        Node fwd=null;
+        Node prev=null;
+        int c=0;
+        Node curr=head;
+        while(curr!=null && c<k)
+        {
+            fwd=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=fwd;
+            c++;
+        }
+        if(fwd!=null)
+        head.next=reverse(fwd,k);
+        return prev;
+    }
+}
+
