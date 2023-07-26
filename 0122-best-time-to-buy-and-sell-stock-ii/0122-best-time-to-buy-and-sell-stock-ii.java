f2253ec31a797e748d152519abe867b5a1f7c592
Riya Jaiswal <84279900+riyajaiswal25@users.noreply.github.com>
2023-07-26 15:10:02 +0530

    Time: 5 ms (7.64%), Space: 47.9 MB (5.23%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length==0)
+            return 0;
+        int n=prices.length;
+        int[][] dp = new int[n+1][2];
+        for(int[] row:dp)
+            Arrays.fill(row,-1);
+        return f(prices,0,0,prices.length,dp);
+    }
+    private int f(int[] prices, int ind, int buy, int n,int[][] dp)
+    {
+        if(ind==n)
+            return dp[ind][buy]=0;
+        int op1, op2;
+        int maxi=0;
+        if(dp[ind][buy]!=-1)
+            return dp[ind][buy];
+        if(buy == 0)
+        {
+            op1 = 0+f(prices,ind+1,0,n,dp);
+            op2 = -prices[ind]+f(prices,ind+1,1,n,dp);
+            maxi = (int)Math.max(op1,op2);
+        }
+        else{
+            op1 = f(prices,ind+1,1,n,dp);
+            op2 = prices[ind]+f(prices,ind+1,0,n,dp);
+            maxi = (int)Math.max(op1,op2);
+        }
+       return dp[ind][buy]=maxi;
+    }
+}
