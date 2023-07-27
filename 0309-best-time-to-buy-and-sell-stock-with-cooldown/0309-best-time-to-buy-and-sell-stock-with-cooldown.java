f12b61589b901b093205bb97c220c2c91a279a01
Riya Jaiswal <84279900+riyajaiswal25@users.noreply.github.com>
2023-07-27 13:04:59 +0530

    Time: 1 ms (84.22%), Space: 41.5 MB (10.82%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,22 +1,32 @@
 class Solution {
     public int maxProfit(int[] prices) {
-         int n = prices.length;
-        if(n == 1)
+        if(prices.length==0)
             return 0;
-        //solve for the last 2 days manually
-        int op1 = Math.max(prices[n - 1], prices[n - 2]), up2 = 0, up1 = Math.max(0, prices[n - 1] - prices[n - 2]);
-        //solve for each day until day 0 is reached
-        for(int day = n - 3; day >= 0; day--) {
-            //calculate today
-            int op0 =Math.max(prices[day] + up2, op1);
-            int up0 = Math.max(op1 - prices[day], up1);
-            //remember today and forget yesterday
-            up2 = up1;
-            up1 = up0;
-            op1 = op0;
+        int n=prices.length;
+        int[][] dp = new int[n+1][2];
+        for(int[] row:dp)
+            Arrays.fill(row,-1);
+        return f(prices,0,0,prices.length,dp);
+    }
+    private int f(int[] prices, int ind, int buy, int n,int[][] dp)
+    {
+        if(ind>=n)
+            return 0;
+        int op1, op2;
+        int maxi=0;
+        if(dp[ind][buy]!=-1)
+            return dp[ind][buy];
+        if(buy == 0)
+        {
+            op1 = 0+f(prices,ind+1,0,n,dp);
+            op2 = -prices[ind]+f(prices,ind+1,1,n,dp);
+            maxi = (int)Math.max(op1,op2);
         }
-        //final value of up1 is the maximum profit when the stock is unowned on day 0
-        return up1;       
-
+        else{
+            op1 = f(prices,ind+1,1,n,dp);
+            op2 = prices[ind]+f(prices,ind+2,0,n,dp);
+            maxi = (int)Math.max(op1,op2);
+        }
+       return dp[ind][buy]=maxi;
     }
 }
