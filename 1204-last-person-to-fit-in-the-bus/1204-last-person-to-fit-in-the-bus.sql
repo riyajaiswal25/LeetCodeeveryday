0a4f0f1c9e29a00d63b637f1fd51d60cf867b489
Riya Jaiswal <84279900+riyajaiswal25@users.noreply.github.com>
2023-10-16 16:33:15 +0530

    Time: 1354 ms (93.07%), Space: 0B (100.00%) - LeetHub

---
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select person_name from
+(select person_name, weight, turn, sum(weight) over(order by turn) as cum_sum from Queue)x
+where cum_sum<=1000
+order by turn desc limit 1
