bf8a864ad38d991a71edd736197ee67837d72f3a
Riya Jaiswal <84279900+riyajaiswal25@users.noreply.github.com>
2023-12-17 07:40:15 +0530

    Time: 11 ms (9.63%), Space: 44.3 MB (7.51%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,28 +1,25 @@
 class Solution {
     public int compress(char[] chars) {
         String s="";
-        int c=1;
+        int freq=1;
         for(int i=0;i<chars.length-1;i++)
         {
             if(chars[i]==chars[i+1])
-                c++;
-            else
-            {
-                if(c>1)
+                freq++;
+            else{
+                if(freq>1)
                 {
                     s+=chars[i];
-                    s+=c;
+                    s+=freq;
                 }
                 else
-                {
                     s+=chars[i];
-                }
-                c=1;
+                freq=1;
             }
         }
         s+=chars[chars.length-1];
-        if(c>1)
-            s+=c;
+        if(freq>1)
+            s+=freq;
         int k=s.length();
         for(int i=0;i<k;i++)
             chars[i]=s.charAt(i);
